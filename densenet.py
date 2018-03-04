3251b1c1e13188694dfaf6fd8cd71caad62240dc
prajjwal1 <prajjwalin@protonmail.com>
2018-03-04 12:29:11 +0530

    densenet

--- densenet.py
+++ densenet.py
@@ -16,14 +16,14 @@ import sys
 import math
 
 class Bottleneck(nn.Module):
-    def __init__(self,nChannels, growRate):
+    def __init__(self,nChannels, growthRate):
         super(Bottleneck, self).__init__()
         interChannels =4*growthRate
         self.bn1 = nn.BatchNorm2d(nChannels)
         self.conv1 = nn.Conv2d(nChannels, interChannels,kernel_size=1,
                 bias=False)
         self.bn2 = nn.BatchNrm2d(interChannels)
-        self.conv2 = self.Conv2d(interChannels, growthRate,kernel_siz=3,
+        self.conv2 = self.Conv2d(interChannels, growthRate,kernel_size=3,
                 padding=1, bias=False)
 
     def forward(self,x):
@@ -45,7 +45,7 @@ class SingleLayer(nn.Module):
         return out
 
 class DenseNet(nn.Module):
-    def __init__(self, growthRate,depth,reduction,nClasses,b0ottleneck):
+    def __init__(self, growthRate,depth,reduction,nClasses,bottleneck):
         super(DenseNet,self).__init__()
 
         nDenseBlocks = depth-4
@@ -53,4 +53,13 @@ class DenseNet(nn.Module):
             nDenseBlocks //=2
 
         nChannels = 2*growthRate
-        self.conv1 = nn.Conv2d(3, nChannels,)
+        self.conv1 = nn.Conv2d(3, nChannels,kernel_size=3,padding=1,
+                bias=False)
+        self.dense1 = self.make_dense(nChannels,growthRate,nDenseBlocks,bottleneck)
+        nChannels += nDenseBlocks*growthRate
+        nOutChannels = int(math.floor(nChannels*reduction))
+        self.trans1 = Transition(nChannels,nOutChannels)
+
+        nChannels = nOutChannels
+        self.dense2 = self.make_dense(nChannels,growthRate,nDenseBlocks,bottlenecbottleneck)
+        nChannels += nDenseBlocks*growthRate
