f0fd9785ad86f7261b751d0e84f3dc651db0a5f1
prajjwal1 <prajjwalin@protonmail.com>
2018-03-14 17:34:34 +0530

    removed typos

--- config.py
+++ config.py
@@ -1,33 +1,17 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import os
 import torch.nn.functional as F
 import torchvision.datasets as datasets
 import torchvision.models as models
 
+from bce import binary_cross_entropy_with_logits
+from densenet import DenseNetBase, DenseNet121, DenseNet100
+from multilabel_dataset import MultiLabelDataset
 
-# In[1]:
-
-
-from binary_cross_entropy import binary_cross_entropy_with_logits
-from DenseNet import DenseNetBase, DenseNet121,DenseNet100
-from dataset import MultiLabelDataset
-
-
-# In[2]:
-
-
-def loss_function(multilabel):
+def get_loss_function(multilabel):
     if multilabel:
         return binary_cross_entropy_with_logits
     return F.cross_entropy
 
-
-# In[4]:
 def get_dataset(name, partition, transform):
     image_folder_datasets = ['imagenet', 'food-101']
     image_dir = os.path.join('input', '%s-%s' % (name, partition))
@@ -44,17 +28,12 @@ def get_dataset(name, partition, transform):
         dataset = MultiLabelDataset(csv_path, image_dir, transform)
     return dataset
 
-
-# In[5]:
-
-
 def get_network(name, num_classes, pretrained):
-    if name=='densenet-base':
+    if name == 'densenet-base':
         assert not pretrained
-        return DenseNetBase(g_rate=12,depth=100,reduction=0.5,bottleneck=True,num_classes=num_classes)
+        return DenseNetBase(growthRate=12, depth=100, reduction=0.5, bottleneck=True, nClasses=num_classes)
     elif name == 'densenet-121':
-        return DenseNet121(num_classes,pretrained)
+        return DenseNet121(num_classes, pretrained) 
     elif name == 'densenet-100':
         assert not pretrained
         return DenseNet100(num_classes)
-
